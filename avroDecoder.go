4b128a9c14a35602c3dc7dfe58a9b298bab7448b
Dejan Simic <desimic@gmail.com>
2017-11-08 23:36:37 +0100

    Use different avro lib
    linkedin's avro lib has a problem with schemas that use default values
    which are not present in the enum list:
    https://github.com/linkedin/goavro/issues/95
    
    Due to design of the lib (there's no concept of decoder/encoder only
    codec) I don't expect this to be fixed soon.

--- avroDecoder.go
+++ avroDecoder.go
@@ -1,77 +1,22 @@
 package tailtopic
 
 import (
-	"encoding/binary"
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-	"strconv"
-	"sync"
-
-	"github.com/linkedin/goavro"
+	avro "github.com/elodina/go-avro"
+	kavro "github.com/elodina/go-kafka-avro"
 )
 
-type schemaRegistryResponse struct {
-	Schema string
+type avroSchemaRegistryDecoder struct {
+	decoder *kavro.KafkaAvroDecoder
 }
 
 func newAvroDecoder(schemaregURI string) decoder {
-	return &avroSchemaRegistryDecoder{
-		schemaregURI,
-		&sync.Mutex{},
-		make(map[uint32]*goavro.Codec)}
-}
-
-type avroSchemaRegistryDecoder struct {
-	schemaregURI string
-	*sync.Mutex
-	codecs map[uint32]*goavro.Codec
-}
-
-func (sr *avroSchemaRegistryDecoder) fetchCodec(version uint32) (*goavro.Codec, error) {
-	sr.Lock()
-	defer sr.Unlock()
-	v, ok := sr.codecs[version]
-	if ok {
-		return v, nil
-	}
-	schemaURL := sr.schemaregURI + "/schemas/ids/" + strconv.Itoa(int(version))
-	resp, err := http.Get(schemaURL)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	schema, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	res := schemaRegistryResponse{}
-	err = json.Unmarshal(schema, &res)
-	if err != nil {
-		return nil, err
-	}
-	codec, err := goavro.NewCodec(res.Schema)
-	if err != nil {
-		return nil, err
-	}
-	sr.codecs[version] = codec
-	return codec, nil
+	return &avroSchemaRegistryDecoder{kavro.NewKafkaAvroDecoder(schemaregURI)}
 }
 
 func (sr *avroSchemaRegistryDecoder) decode(msg []byte) (string, error) {
-	version := binary.BigEndian.Uint32(msg[1:5])
-	codec, err := sr.fetchCodec(version)
+	decodedRecord, err := sr.decoder.Decode(msg)
 	if err != nil {
 		return "", err
 	}
-	n, _, err := codec.NativeFromBinary(msg[5:])
-	if err != nil {
-		return "", err
-	}
-
-	jsonBytes, err := json.Marshal(n)
-	if err != nil {
-		return "", err
-	}
-	return string(jsonBytes), nil
+	return (decodedRecord.(*avro.GenericRecord)).String(), nil
 }
